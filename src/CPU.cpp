6156cd306ff88f6db95daaa5c0cc3b29f6559b2b
Raisess <danilosantana456789@gmail.com>
2022-10-15 15:40:36 -0300

    CPU: improve reset method

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -8,14 +8,14 @@ Open6502::CPU::CPU(const std::shared_ptr<DataBus>& data_bus)
 Open6502::CPU::~CPU() {}
 
 void Open6502::CPU::reset() {
+  this->data_bus->reset();
+  this->stack.reset();
   this->status = Status::INTERRUPT_DISABLED;
   this->program_counter = 0x0000;
-  this->stack_pointer = 0x00;
+  this->stack_pointer = this->stack.get();
   this->accumulator = 0x00;
   this->x = 0x00;
   this->y = 0x00;
-  this->data_bus->reset();
-  this->stack.reset();
 }
 
 Open6502::CPU::Status Open6502::CPU::get_status() const {
