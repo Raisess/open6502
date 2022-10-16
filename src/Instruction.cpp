5fb6b90d612f800d801ae7b778fec14baa646ef1
Raisess <danilosantana456789@gmail.com>
2022-10-16 15:12:47 -0300

    Instruction: add immediate load opcodes

--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -7,21 +7,62 @@ void Open6502::Instruction::Fecth(uint8_t opcode, CPU* cpu, std::shared_ptr<Data
     case BRK::Opcode:
       BRK().execute(cpu, data_bus);
       break;
+    case LDYI::Opcode:
+      LDYI().execute(cpu, data_bus);
+      break;
+    case LDXI::Opcode:
+      LDXI().execute(cpu, data_bus);
+      break;
     case LDAI::Opcode:
       LDAI().execute(cpu, data_bus);
+      break;
+    default:
+      std::cerr << "Error: Instruction not implemented: " << std::hex << unsigned(opcode) << std::dec << std::endl;
+      exit(1);
       break;
   }
 }
 
 // TODO: in progress
 void Open6502::BRK::execute(CPU* cpu, std::shared_ptr<DataBus> data_bus) {
-  std::cout << "BRK" << std::endl;
-  exit(0);
+  cpu->program_counter = 0xFFFE;
 }
 
-// TODO: in progress
+void Open6502::LDYI::execute(CPU* cpu, std::shared_ptr<DataBus> data_bus) {
+  for (uint8_t i = 0; i < LDYI::ClockCycles; i++) {
+    switch (i) {
+      case 0:
+        cpu->program_counter++;
+      case 1:
+        auto addr = data_bus->read(cpu->program_counter);
+        cpu->y = addr;
+        if (cpu->y == 0x00) cpu->status = CPU::Status::ZERO;
+    }
+  }
+}
+
+void Open6502::LDXI::execute(CPU* cpu, std::shared_ptr<DataBus> data_bus) {
+  for (uint8_t i = 0; i < LDXI::ClockCycles; i++) {
+    switch (i) {
+      case 0:
+        cpu->program_counter++;
+      case 1:
+        auto addr = data_bus->read(cpu->program_counter);
+        cpu->x = addr;
+        if (cpu->x == 0x00) cpu->status = CPU::Status::ZERO;
+    }
+  }
+}
+
 void Open6502::LDAI::execute(CPU* cpu, std::shared_ptr<DataBus> data_bus) {
-  std::cout << "LDA Immediate" << std::endl;
-  auto argument = data_bus->read(cpu->program_counter);
-  cpu->accumulator = argument;
+  for (uint8_t i = 0; i < LDAI::ClockCycles; i++) {
+    switch (i) {
+      case 0:
+        cpu->program_counter++;
+      case 1:
+        auto addr = data_bus->read(cpu->program_counter);
+        cpu->accumulator = addr;
+        if (cpu->accumulator == 0x00) cpu->status = CPU::Status::ZERO;
+    }
+  }
 }
--- src/Instruction.h
+++ src/Instruction.h
@@ -13,15 +13,39 @@ public:
   virtual void execute(CPU*, std::shared_ptr<DataBus>) = 0;
 };
 
+// Interrupt Request
+// BRK
 class BRK : public Instruction {
 public:
   static const uint8_t Opcode = 0x00;
+  static const uint8_t ClockCycles = 1;
   void execute(CPU*, std::shared_ptr<DataBus>) final override;
 };
 
+// Load Y Register (Immediate)
+// LDY #$05
+class LDYI : public Instruction {
+public:
+  static const uint8_t Opcode = 0xA0;
+  static const uint8_t ClockCycles = 2;
+  void execute(CPU*, std::shared_ptr<DataBus>) final override;
+};
+
+// Load X Register (Immediate)
+// LDX #$05
+class LDXI : public Instruction {
+public:
+  static const uint8_t Opcode = 0xA2;
+  static const uint8_t ClockCycles = 2;
+  void execute(CPU*, std::shared_ptr<DataBus>) final override;
+};
+
+// Load Accumulator (Immediate)
+// LDA #$05
 class LDAI : public Instruction {
 public:
   static const uint8_t Opcode = 0xA9;
+  static const uint8_t ClockCycles = 2;
   void execute(CPU*, std::shared_ptr<DataBus>) final override;
 };
 
